5f814106a9090f70854a2f273abe4de0bd177653
Etienne Rossignon <etienne.rossignon@gadz.org>
2015-04-10 08:12:48 +0200

    #1 : windows implementation

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 var shell = require("shelljs");
 
 module.exports = function(callback){
-  var cb = callback ? function(code, output){
-    if(code !== 0){
-      callback(new Error("command exited with code: " + code + " output: " + output));
-    }
-    callback(null, output.trim());
-  } : undefined;
 
-  if(cb){
-    shell.exec("hostname -f", { silent: true }, cb)
-  }
-  else {
-    return shell.exec("hostname -f", { silent: true }).output.trim();
+    var cb = callback ? function(code, output){
+        if(code !== 0){
+            callback(new Error("command exited with code: " + code + " output: " + output));
+        }
+        callback(null, output.trim());
+    } : undefined;
+
+  if (process.platform === "win32") {
+
+      // http://serverfault.com/a/73643/251863
+      var env = process.env;
+      var fqdn= env.COMPUTERNAME + ( (env.USERDNSDOMAIN && env.USERDNSDOMAIN.length >0) ? "." + env.USERDNSDOMAIN: "");
+      if (!callback) {
+          return fqdn
+      } else {
+          callback(null,fqdn);
+      }
+  }  else {
+
+      if(cb){
+          shell.exec("hostname -f", { silent: true }, cb)
+      }
+      else {
+          return shell.exec("hostname -f", { silent: true }).output.trim();
+      }
+
   }
 };
